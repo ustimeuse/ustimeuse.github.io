f59133e1fdf2adfc5e7acb1a04c374137ea298b3
Yuqi Hou <yuqi.hou@gmail.com>
2016-04-16 23:26:13 -0400

    main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,12 @@
 // use the Queue.js library to read multiple files
 queue()
-  .defer(d3.csv, "data/atus_rost0314_small.csv")
-  .defer(d3.csv, "data/atus_resp0314_small.csv")
-  .defer(d3.csv, "data/atussum_0314_small.csv")
-  .defer(d3.csv, "data/atus_act0314_small.csv")
+  .defer(d3.csv, "data/filtered-data.csv")
+  .defer(d3.csv, "data/final-data.csv")
+  .defer(d3.csv, "data/datatouse.csv")
   .await(analyze);
 
-function analyze(error, roster, response, summary, activity){
-  console.log("Roster");
-  console.log(roster);
-  console.log("Response");
-  console.log(response);
-  console.log("Summary");
-  console.log(summary);
-  console.log("Activity");
-  console.log(activity);
+function analyze(error, filteredData, allData, data){
+  console.log(filteredData);
+  console.log(allData);
+  console.log(data);
 }
