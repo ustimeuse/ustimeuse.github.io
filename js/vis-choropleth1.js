3e73658f77f7d5f3cc2797b628aa2edbb58a5820
bluedolphinx1 <tle@college.harvard.edu>
2016-04-25 20:09:35 -0400

    new choropleth

--- js/vis-choropleth1.js
+++ js/vis-choropleth1.js
@@ -3,8 +3,8 @@
  */
 
 // --> CREATE SVG DRAWING AREA
-var width = 1000,
-    height = 400;
+var width = 2000,
+    height = 1000;
 
 var USmap;
 var timeuse;
@@ -14,16 +14,12 @@ var dataByStates = d3.map();
 
 queue()
     .defer(d3.json, "data/us-geo.json")
-    .defer(d3.csv, "data/choropleth.csv", processData)
+    .defer(d3.csv, "data/choropleth_new.csv", processData)
     .await(loaded);
 
 var svg = d3.select("#visualization").append("svg")
     .attr("width", width)
     .attr("height", height)
-
-var svg1 = d3.select("#visualization1").append("svg")
-    .attr("width", width)
-    .attr("height", height);
 
 
 function processData(d) {
@@ -40,22 +36,26 @@ function loaded(error,map,data) {
 }
 
 var projection = d3.geo.albersUsa()
-    .scale(500)
-    .translate([width/3, height /4]);
+    .scale(1000)
+    .translate([width/2.5, height /4]);
 
 var path = d3.geo.path()
     .projection(projection);
 
 
 // Create color scale
-var colors = ["#fdbb84","#fc8d59","#e34a33","#b30000"];
+var colors = ["#fef0d9","#fdd49e","#fdbb84","#fc8d59"];
 // set up a scale that can take data values as input, and will return colors
 var color = d3.scale.quantize()
   .range(colors);
-var color1 = d3.scale.quantize()
-    .range(colors);
 
-console.log("Hi");
+
+// Upon hovering, show text
+function getText(d,selectedValue) {
+    format = d3.format("0,000");
+    console.log(d);
+    var temp=d.properties[selectedValue];
+}
 
 function updateMap(){
 
@@ -72,21 +72,16 @@ function updateMap(){
     // Get selected value
     selectedValue=d3.select("#map-type").property("value");
     // Get selected value
-    selectedValue1=d3.select("#map-type1").property("value");
 
     var min=d3.min(timeuse, function(d) {return +d[selectedValue]})
     var max=d3.max(timeuse, function(d) {return +d[selectedValue]})
-    var min1=d3.min(timeuse, function(d) {return +d[selectedValue1]})
-    var max1=d3.max(timeuse, function(d) {return +d[selectedValue1]})
 
     // Pass in domain for color scale
     //colorScale.domain(d3.range(min, max, (max-min)/colors.length));
     color.domain(d3.range(min, max, (max-min)/colors.length));
-    color1.domain(d3.range(min1, max1, (max1-min1)/colors.length));
 
     // Save these labels for legend
     var leg_labels=d3.range(min, max, (max-min)/colors.length);
-    var leg_labels1=d3.range(min1, max1, (max1-min1)/colors.length);
     var US = USmap.features
 
     // Reference: http://chimera.labs.oreilly.com/books/1230000000345/ch12.html#_choropleth
@@ -103,12 +98,20 @@ function updateMap(){
             if (dataCode == jsonCode) {
                 //Copy the data value into the JSON
                 US[j].properties[selectedValue]= timeuse[i][selectedValue];
-                US[j].properties[selectedValue1]= timeuse[i][selectedValue1];
                 //Stop looking through the JSON
                 break;
             }
         }
     }
+
+    // Draw tip
+    tip1 = d3.tip().attr('class', 'd3-tip').html(function(d) {
+        console.log(selectedValue)
+        return (getText(d,selectedValue));
+    });
+
+    svg.call(tip1)
+
     //console.log(US);
     svg.selectAll('path.countries')
         .data(US)
@@ -117,19 +120,12 @@ function updateMap(){
         .attr('class', 'countries')
         .attr('d', path)
         .attr('fill', function(d,i) {
-            return color(d.properties[selectedValue]);
+            if(!isNaN(d.properties[selectedValue])){
+                return color(d.properties[selectedValue]);
+            }
+            return "#e5e5e5";
         })
 
-    //console.log(US);
-    svg1.selectAll('path.countries')
-        .data(US)
-        .enter()
-        .append('path')
-        .attr('class', 'countries')
-        .attr('d', path)
-        .attr('fill', function(d,i) {
-            return color1(d.properties[selectedValue1]);
-        })
 
     // Create legend
     var legend = svg.selectAll('rect')
@@ -147,21 +143,18 @@ function updateMap(){
             return color(d);
         });
 
-    // Create legend
-    var legend1 = svg1.selectAll('rect')
-        .data(leg_labels1)
+
+    // Draw gray box for no data
+    legend
+        .data("No data")
         .enter()
         .append('rect')
         .attr("class", "rectangles")
-        .attr("x", 540)
-        .attr("y", function(d, i){
-            return i*40;
-        })
+        .attr("x", 0)
+        .attr("y", leg_labels.length*40)
         .attr("width", 20)
         .attr("height", 20)
-        .style("fill", function(d){
-            return color1(d);
-        });
+        .style("fill", "#e5e5e5")
 
     // Add labels for legend
     svg.selectAll("text")
@@ -181,23 +174,16 @@ function updateMap(){
             return ((format(leg_labels[i])) + "-" + (format(max)));
         });
 
-    // Add labels for legend
-    svg1.selectAll("text")
-        .data(leg_labels1)
+    // Append No Data in Legend
+    svg.selectAll("text")
+        .data("No data")
         .enter()
-        .append('text')
+        .append("text")
         .attr("class", "legend-labels")
-        .attr("x", 580)
-        .attr("y", function(d, i) {
-            return i*40+15;
-        })
-        .text(function(d,i) {
-            format=d3.format(".1f")
-            if(i<(leg_labels1.length-1)){
-                return ((format(leg_labels1[i]))+ "-" + (format(leg_labels1[i+1])));
-            }
-            return ((format(leg_labels1[i])) + "-" + (format(max1)));
-        });
+        .attr("x", 40)
+        .attr("y", leg_labels.length*40 + 15)
+        .text("No data")
+
 
 
 }
