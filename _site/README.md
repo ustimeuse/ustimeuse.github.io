9cb9c4f8511e082e96b6e56303ff6fc81263302c
Yuqi Hou <yuqi.hou@gmail.com>
2016-04-30 20:36:54 -0400

    Refactored to use jekyll

---
+++ _site/README.md
@@ -0,0 +1,21 @@
+# CS 171 Final Project
+Yuqi Hou, Maria Lai, Tuongvan Le
+Spring 2016
+
+## American Time Use: Who Spends How Long at What
+
+### Abstract
+We are interested in seeing how Americans use their time and how the hours spent on different activities have changed over time. Our goal is to understand how time spent on daily activities differs by gender, income, and age. We also would like to examine whether external factors (such as unemployment rates) influence how time is spent on daily activities by average Americans of different demographics groups. 
+
+### Data
+The dataset we plan to use is the American Time Use Survey from 2003-2014 provided by the Bureau of Labor Statistics. This data is freely downloadable from the website http://www.bls.gov/tus/. The dataset includes information on survey respondent's labor force status, earnings, total time spent doing each activity (as coded by the BLS), number of household members and non-household members, age, and sex. For unemployment rates, we will use Labor Force Statistics from the Current Population Survey which is also publicly available from the BLS.
+
+### Goals
+A few questions that we would like to answer include: 
+- How does a typical American spend his or her day?‘
+- How do factors like income, education, age, and gender affect the leisure/work time distribution?
+- How has people’s use of time changed during and after the recession?
+- Which activities take the most time in a person's day? 
+
+### Credits
+Site based on Start Bootstrap. Start Bootstrap was created by and is maintained by **[David Miller](http://davidmiller.io/)**, Owner of [Blackrock Digital](http://blackrockdigital.io/).
---
+++ _site/js/stackedAreaChart.js
@@ -0,0 +1,180 @@
+
+
+/*
+ * StackedAreaChart - Object constructor function
+ * @param _parentElement 	-- the HTML element in which to draw the visualization
+ * @param _data						-- the
+ */
+
+StackedAreaChart = function(_parentElement, _data){
+  this.parentElement = _parentElement;
+  this.data = _data.sort(function(a, b) {
+    return d3.descending(a.age, b.age);
+  });
+  this.displayData = []; // see data wrangling
+
+  // DEBUG RAW DATA
+  //console.log(this.data);
+
+  this.initVis();
+}
+
+
+
+/*
+ * Initialize visualization (static content, e.g. SVG area or axes)
+ */
+
+StackedAreaChart.prototype.initVis = function(){
+  var vis = this;
+
+  vis.margin = { top: 40, right: 0, bottom: 60, left: 60 };
+
+  vis.width = 800 - vis.margin.left - vis.margin.right,
+    vis.height = 400 - vis.margin.top - vis.margin.bottom;
+
+
+  // SVG drawing area
+  vis.svg = d3.select("#" + vis.parentElement).append("svg")
+    .attr("width", vis.width + vis.margin.left + vis.margin.right)
+    .attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+
+  // TO-DO: Overlay with path clipping
+  vis.svg.append("defs").append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("width", vis.width)
+    .attr("height", vis.height);
+
+
+  // Scales and axes
+  vis.x = d3.scale.linear()
+    .range([0, vis.width])
+    .domain(d3.extent(vis.data, function(d) { return d.age; }));
+
+  vis.y = d3.scale.linear()
+    .range([vis.height, 0]);
+
+  vis.xAxis = d3.svg.axis()
+    .scale(vis.x)
+    .orient("bottom");
+
+  vis.yAxis = d3.svg.axis()
+    .scale(vis.y)
+    .orient("left");
+
+  vis.svg.append("g")
+    .attr("class", "x-axis axis")
+    .attr("transform", "translate(0," + vis.height + ")");
+
+  vis.svg.append("g")
+    .attr("class", "y-axis axis");
+
+
+  // TO-DO: Initialize stack layout
+  var stack = d3.layout.stack()
+    .values(function(d) { return d.values; })
+    .order(["inside-out"]);
+
+  // Transpose the data
+  var dataCategories = colorScale.domain();
+
+  // Rearrange data into layers
+  var transposedData = dataCategories.map(function(name) {
+    return {
+      name: name,
+      values: vis.data.map(function(d) {
+        return {age: d.age, y: d[name]};
+      })
+    };
+  });
+
+  vis.stackedData = stack(transposedData);
+
+  // TO-DO: Stacked area layout
+  vis.area = d3.svg.area()
+    //.interpolate("step-before")
+    .interpolate("linear")
+    .x(function(d) {return vis.x(d.age); })
+    .y0(function(d) { return vis.y(d.y0); })
+    .y1(function(d) { return vis.y(d.y0 + d.y); });
+
+  // TO-DO: Tooltip placeholder
+  vis.tooltip = vis.svg.append("text")
+    .attr("x", 10)
+    .attr("y", 10);
+
+  // TO-DO: (Filter, aggregate, modify data)
+  vis.wrangleData();
+}
+
+
+
+/*
+ * Data wrangling
+ */
+
+StackedAreaChart.prototype.wrangleData = function(){
+  var vis = this;
+
+  // In the first step no data wrangling/filtering needed
+  vis.displayData = vis.stackedData;
+
+  // Update the visualization
+  vis.updateVis();
+}
+
+
+
+/*
+ * The drawing function - should use the D3 update sequence (enter, update, exit)
+ * Function parameters only needed if different kinds of updates are needed
+ */
+
+StackedAreaChart.prototype.updateVis = function(){
+  var vis = this;
+
+  // Update domain
+  // Get the maximum of the multi-dimensional array or in other words, get the highest peak of the uppermost layer
+  vis.y.domain([0, d3.max(vis.displayData, function(d) {
+    return d3.max(d.values, function(e) {
+      return e.y0 + e.y;
+    });
+  })
+  ]);
+
+
+  // Draw the layers
+  var categories = vis.svg.selectAll(".area")
+    .data(vis.displayData);
+
+  categories.enter().append("path")
+    .attr("class", "area")
+    .on('mouseover', function(d) {
+      vis.tooltip.text(d.name);
+    })
+    .on('mouseout',function(d) {
+      vis.tooltip.text("");
+    });
+
+  categories
+    .style("fill", function(d) {
+      console.log(colorScale(d.name));
+      return colorScale(d.name);
+    })
+    .attr("d", function(d) {
+      //console.log(vis.area(d.values));
+      return vis.area(d.values);
+    })
+
+  // TO-DO: Update tooltip text
+
+  categories.exit().remove();
+
+
+  // Call axis functions with the new domain
+  vis.svg.select(".x-axis").call(vis.xAxis);
+  vis.svg.select(".y-axis").call(vis.yAxis);
+}
